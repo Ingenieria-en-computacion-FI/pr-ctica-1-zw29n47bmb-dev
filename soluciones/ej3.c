482f046bf6e8cbf2618c3ad011f9c06cc453cff7
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-02-19 21:53:11 -0600

    Add memory address printing for arrays a, b, and c
    Updated the program to print the memory addresses of three arrays: a, b, and c.

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -2,12 +2,21 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 3
-     int m[2][4];
+     int a[3];
+    int b[3];
+    int c[3];
 
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 4; j++){
-            printf("&m[%d][%d] = %p\n", i, j, (void*)&m[i][j]);
-        }
-    }
+    printf("Direcciones de a:\n");
+    for(int i = 0; i < 3; i++)
+        printf("&a[%d] = %p\n", i, (void*)&a[i]);
+
+    printf("\nDirecciones de b:\n");
+    for(int i = 0; i < 3; i++)
+        printf("&b[%d] = %p\n", i, (void*)&b[i]);
+
+    printf("\nDirecciones de c:\n");
+    for(int i = 0; i < 3; i++)
+        printf("&c[%d] = %p\n", i, (void*)&c[i]);
+
     return 0;
 }
