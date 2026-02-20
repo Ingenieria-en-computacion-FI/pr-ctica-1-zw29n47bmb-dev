c5b952ce79a485c278802aa6cf075e6fe7dccfd5
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-02-19 22:02:19 -0600

    Update ej9.c

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 9
+     int a[5];
+
+    printf("&a[-1] = %p\n",(void*)&a[-1]);
+    printf("&a[5] = %p\n",(void*)&a[5]);
+
+    printf("Compila pero es comportamiento indefinido\n");
     return 0;
 }
